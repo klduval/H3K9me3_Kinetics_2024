856065c87410946ea71dfdd63794532690c0a18e
Katie <kld57880@uga.edu>
2024-02-08 14:13:38 -0500

    Update 4h_24h_peak_amp.R

--- 4h_24h_peak_amp.R
+++ 4h_24h_peak_amp.R
@@ -5,7 +5,7 @@ fill_colors <- c("steelblue","gray83", "skyblue3","lightcyan3", "gray60", "light
 
 
 ##read in peak height info####
-peak_amplitude <- read_delim("/Users/mangofrog7/Documents/Graduate_School/Projects/bioinformatics/K9anti_CnR_5.2022/4.5h_24h_shared_peaks_matrix.tab", delim = '\t', skip = 2) %>% 
+peak_amplitude <- read_delim("4.5h_24h_shared_peaks_matrix.tab", delim = '\t', skip = 2) %>%
   mutate_if(is.factor, ~as.numeric(as.character(.)))
 
 ###assign unique peak numbers###
@@ -19,22 +19,21 @@ amp_24h_scores <- amp_24h %>% rowwise(ID) %>% summarise(amp = rowSums(across(whe
 
 total_amp <- bind_rows(amp_4.5h_scores, amp_24h_scores) %>% mutate(timepoint = factor(timepoint, levels = c("4.5h", "24h"))) %>% filter(!is.na(amp))
 
-amp_noOutliers <- total_amp %>% 
-  group_by(timepoint) %>% 
-  mutate(zPW = scale(amp)) %>% 
+amp_noOutliers <- total_amp %>%
+  group_by(timepoint) %>%
+  mutate(zPW = scale(amp)) %>%
   filter(between(zPW,-3,+3)) %>%
   mutate(timepoint = factor(timepoint, levels = c("4.5h", "24h")))
 
 K9_mat_amp_diff <- ggplot(amp_noOutliers, aes(x = timepoint, y = amp)) +
-  geom_violin(trim=FALSE, scale="width", aes(fill=timepoint)) + 
-  ylim(0,5000) + labs(y = "Average H3K9me3 Signal") + 
+  geom_violin(trim=FALSE, scale="width", aes(fill=timepoint)) +
+  ylim(0,5000) + labs(y = "Average H3K9me3 Signal") +
   theme_minimal() + scale_fill_manual(values = fill_colors) +
-  geom_boxplot(width=0.1, outlier.shape=NA, aes(fill=timepoint)) + 
+  geom_boxplot(width=0.1, outlier.shape=NA, aes(fill=timepoint)) +
   theme(legend.position="none") +
-  theme(text = element_text(size = 8)) + 
-  theme(axis.title.x = element_blank()) 
-ggsave("/Users/mangofrog7/Documents/Graduate_School/Papers/K9_Kinetics/FigS10/TEprops_4.5hpf_pie.pdf", plot = K9_mat_amp_diff, width = 2, height = 3, units = "in")
+  theme(text = element_text(size = 8)) +
+  theme(axis.title.x = element_blank())
+ggsave("K9_Kinetics/FigS10/TEprops_4.5hpf_pie.pdf", plot = K9_mat_amp_diff, width = 2, height = 3, units = "in")
 
 total_amp %>% group_by(timepoint) %>% summarise(mean_amp = mean(amp))
 total_amp %>% group_by(timepoint) %>% summarise(med_amp = median(amp))
-
