2d67ce396981d0de8729d85cea1be41f34de550f
Katie <kld57880@uga.edu>
2024-02-08 14:14:08 -0500

    Update TEMap_Intersect.R

--- TEMap_Intersect.R
+++ TEMap_Intersect.R
@@ -3,12 +3,12 @@ library("RColorBrewer")
 library("dplyr")
 
 
-TE_35bp_scores <- read.delim("/Users/mangofrog7/Documents/Graduate_School/Projects/bioinformatics/general/TE_35bp_coords_score.bed", 
+TE_35bp_scores <- read.delim("TE_35bp_coords_score.bed",
                               col.names = c("chr", "start", "end", "score"))
-TE_ann <- read.delim("/Users/mangofrog7/Documents/Graduate_School/Projects/bioinformatics/general/TEann_final.bed", 
+TE_ann <- read.delim("TEann_final.bed",
                              col.names = c("chr", "start", "end", "name"))
 
 TEann_w35bp_mapscores <- left_join(TE_ann, TE_35bp_scores, multiple = "first")
 
-write.table(TEann_w35bp_mapscores, "/Users/mangofrog7/Documents/Graduate_School/Projects/bioinformatics/general/TEann_w35bp_mapscores.bed", 
+write.table(TEann_w35bp_mapscores, "TEann_w35bp_mapscores.bed",
             row.names = FALSE, col.names = FALSE, sep = "\t", quote = FALSE)
