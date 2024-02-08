dd141027fad8466aa74e24e176067d01fd1c0fe7
Katie <kld57880@uga.edu>
2024-02-08 14:13:54 -0500

    Update periC_assign.sh

--- periC_assign.sh
+++ periC_assign.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-#SBATCH --job-name=job
-#SBATCH --partition=batch
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=24
-#SBATCH --mem=80gb
-#SBATCH --time=24:00:00
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=kld57880@uga.edu
-
-BASEDIR="/scratch/kld57880/TC_final"
-
 module load BLAST+/2.12.0-gompi-2020b
 makeblastdb -in $BASEDIR/danio_refseq.fa -dbtype nucl
 
@@ -22,10 +11,8 @@ cat sat1_blast_trimmed.txt | awk '{print $2 "\t" $9 "\t" $10}' > sat1_trimmed.be
 
 ####manually created bed file with rough calls of centromeres based on where the highest density of SAT1 repeats were called
 ###to broaded these regions I'm going to use bedtools slop
-
 ml BEDTools
 
-##going to do this again but now for 1000 replicates
 for ((i=1;i<=1000;i++));
 do
   bedtools shuffle -chrom -noOverlapping -i $BASEDIR/peric/centromeres_sloppy2.bed -excl $BASEDIR/peric/centromeres_sloppy2.bed -g $BASEDIR/genome/chrNameLength.txt > $BASEDIR/peric/null/NULLcoords2_"$i".bed
